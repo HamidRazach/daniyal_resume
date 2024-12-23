0679c9213577be7eba471b11025d54744e77fb8b
hamidRazach <150281909+HamidRazach@users.noreply.github.com>
2024-12-23 22:34:59 +0500

    container remove

--- src/component/header.js
+++ src/component/header.js
@@ -1,54 +1,68 @@
-import React from 'react';
-import { header_logo } from '../constant/images';
+import React from "react";
+import { header_logo } from "../constant/images";
 const Header = () => {
   return (
-    <header className="main_nav">
-      <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light w-100">
-          <a className="navbar-brand p_0" href="#top">
-            <img src={ header_logo } className="nav-logo"  alt="logo" />
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="#top">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#about">About</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#skill">Skill</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#services">Services</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#portfolio">Portfolio</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#testimonial">Testimonial</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#blog">Blog</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#contact">Contact</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-light w-100">
+        <a className="navbar-brand p_0" href="#top">
+          <img src={header_logo} className="nav-logo" alt="logo" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#top">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#about">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#skill">
+                Skill
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#services">
+                Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#portfolio">
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#testimonial">
+                Testimonial
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#blog">
+                Blog
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </header>
   );
 };
